da8cc9206ce7d79e09c5db00d185581003bb440b
Amir Farhadi <farhaadi.amir@gmail.com>
2024-05-07 09:51:25 -0400

    feat: added resourse for the credentials

--- 0-locals.tf
+++ 0-locals.tf
@@ -1,4 +1,9 @@
+data "external" "fetch_credentials" {
+  program = ["bash", "${path.module}/fetch_credentials.sh"]
+}
+
 locals {
+  credentials = data.external.fetch_credentials.result
   project_id = "caps-mirror-trading-bot"
   region     = "me-central1"
 }
